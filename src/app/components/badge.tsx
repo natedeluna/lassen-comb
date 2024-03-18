675cf6b0c73478ddd40e572383b12feefcf214a1
Nate De Luna <nate.d.luna@gmail.com>
2024-03-18 14:26:34 -0500

    created badge componenet with vertical carosel text

--- src/app/components/badge.tsx
+++ src/app/components/badge.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+"use client"
 
+import React, { useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 interface BadgeProps {
-    text: string;
+    texts: string[];
     color: string;
 }
 
-const Badge: React.FC<BadgeProps> = ({ text, color }) => {
+const Badge: React.FC<BadgeProps> = ({ texts, color }) => {
+    const [index, setIndex] = useState(0);
+    const [badgeWidth, setBadgeWidth] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        }, 10000);
+        return () => clearInterval(interval);
+    }, [texts]);
+
     return (
-        <div style={{ backgroundColor: color, }}
-            className='rounded-full px-2 py-1 border my-2 text-sm'>
-            {text}
-        </div>
+        <motion.div style={{ backgroundColor: color, }}
+            className='flex flex-col items-center justify-center rounded-full px-2 py-1 border my-2 text-sm '
+            layout
+            transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+            >
+            <AnimatePresence mode='wait'>
+                <motion.span
+                    key={texts[index]}
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 10 }}
+                    transition={{ type: 'spring',bounce: .05, stiffness: 450, damping: 30}}
+                >
+                    {texts[index]}
+                </motion.span>
+            </AnimatePresence>
+        </motion.div>
     );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,14 @@ import ScrollTextAppear from "@/app/components/scroll_text_appear";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center pt-[120px] max-w-[500px] mx-auto">
-      <Badge text="Design subscription service" color="whitesmoke" />
+      <Badge texts={["Design subscription", "Available"]} color="whitesmoke" />
       <h1 className="hero_1_title max-w-3xl">Design supplier based out of Austin, TX
       </h1>
       <section>
         <div className="my-12"></div>
         <ScrollTextAppear 
-          text="We are a design supplier based out of Austin, TX. We use a crossfunctional apporach of design and engineering to deliver world 
-          class experiences and products. We are a supplier of design services and products. We are a supplier of design services and products.
-          class experiences and products. We are a supplier of design services and products. We are a supplier of design services and products.
-          class experiences and products. We are a supplier of design services and products. We are a supplier of design services and products.
-          " 
+          text="We use a crossfunctional approach of design and engineering to craft timeless and powerful aesthetics.
+                " 
           />
         <div className="my-12"></div>
       </section>
