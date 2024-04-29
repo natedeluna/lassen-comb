2635362a080091346f28195627b6ce67cdc9017b
Nate De Luna <nate.d.luna@gmail.com>
2024-04-29 15:27:45 -0500

    more showcase improvements

--- src/app/components/parallax_showcase.tsx
+++ src/app/components/parallax_showcase.tsx
@@ -46,39 +46,44 @@ const ParallaxShowcase: React.FC<ParallaxShowcaseProps> = ({ className }) => {
     <div
       ref={showcaseRef}
       style={{ width: windowWidth - 16, transform: 'translateX(-50%)', left: '50%' }}
-      className={`fullWidthOverride relative ${className} flex gap-2 justify-center items-center box-border`}
+      className={`fullWidthOverride relative ${className} flex gap-[5vw] justify-center items-center box-border max-w-[1000px]`}
     >
-      <div className="left-column flex flex-col gap-2 flex-1">
-        <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm ">
-            <Image src={'/images/concept1.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
+      <div className="left-column flex flex-col gap-[4vw] flex-1 ">
+        <div className="card h-fit overflow-hidden ">
+            <Image src={'/images/charlie.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%', margin: 'auto', scale: '.5'}} />
         </div>
-        <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm ">
-            <Image src={'/images/grid2.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
+        <div className="card h-fit overflow-hidden  ">
+            <Image src={'/images/grid2.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%', }} />
         </div>
-        <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm ">
-            <Image src={'/images/logo_consulting_1.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
+        <div className="card h-fit ">
+            <Image src={'/images/logo_consulting_1.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%', scale: ''}} className=''/>
         </div>
         <div className='sm:hidden flex flex-col gap-2'>
-          <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm ">
+          <div className="card h-fit overflow-hidden ">
               <Image src={'/images/tabs.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
           </div>
-          <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm">
+          <div className="card h-fit overflow-hidden ">
             <video src="/images/Parchment - 22 April 2024.mp4" autoPlay loop muted playsInline
               style={{pointerEvents: 'none', touchAction:'none'}}
             ></video>
           </div>
-
+          <div className="card h-fit overflow-hidden ">
+              <Image src={'/images/parchment_ui.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
+          </div>
         </div>
       </div>
 
-      <div className="right-column flex-col gap-2 mt-12 flex-1 sm:flex hidden">
-        <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm ">
+      <div className="right-column flex-col gap-[1vw] mt-12 flex-1 sm:flex hidden">
+        <div className="card h-fit overflow-hidden  ">
             <Image src={'/images/tabs.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
         </div>
-        <div className="card h-fit rounded-lg overflow-hidden border-slate-200 border-[.1px] shadow-sm ">
+        <div className="card h-fit overflow-hidden  ">
           <video src="/images/Parchment - 22 April 2024.mp4" autoPlay loop muted playsInline
             style={{pointerEvents: 'none', touchAction:'none'}}
           ></video>
+        </div>
+        <div className="card h-fit overflow-hidden ">
+            <Image src={'/images/parchment_ui.png'} alt='' width={1000} height={1000} style={{width: '100%', height: '100%'}} />
         </div>
       </div>
     </div>
