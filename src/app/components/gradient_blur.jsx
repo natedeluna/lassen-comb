7058f6865ae84e5cd79369ed5cca17c23abe6a00
Nate De Luna <nate.d.luna@gmail.com>
2024-05-01 11:43:03 -0500

    i think this works

--- src/app/components/gradient_blur.jsx
+++ src/app/components/gradient_blur.jsx
@@ -12,7 +12,7 @@ const GradientBlur = () => {
         bottom: "auto",
         height: "50px",
         pointerEvents: "none",
-        transform: "rotate(180deg)",
+        transform: "rotate(180deg)"
       }}
     >
       <div
@@ -24,11 +24,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 1,
           backdropFilter: "blur(0.5px)",
-          WebkitBackdropFilter: "blur(0.5px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 0) 37.5%)",
+          WebkitBackdropFilter: "blur(0.5px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 0) 100%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 0) 37.5%)",
         }}
       />
       <div
@@ -40,11 +38,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 2,
           backdropFilter: "blur(1px)",
-          WebkitBackdropFilter: "blur(1px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 0) 50%)",
+          WebkitBackdropFilter: "blur(1px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 0) 87.5%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 0) 50%)",
         }}
       />
       <div
@@ -56,11 +52,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 3,
           backdropFilter: "blur(2px)",
-          WebkitBackdropFilter: "blur(2px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 62.5%)",
+          WebkitBackdropFilter: "blur(2px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 0) 75%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 62.5%)",
         }}
       />
       <div
@@ -72,11 +66,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 4,
           backdropFilter: "blur(4px)",
-          WebkitBackdropFilter: "blur(4px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 0) 75%)",
+          WebkitBackdropFilter: "blur(4px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 62.5%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 37.5%, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 0) 75%)",
         }}
       />
       <div
@@ -88,11 +80,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 5,
           backdropFilter: "blur(6px)",
-          WebkitBackdropFilter: "blur(6px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 0) 87.5%)",
+          WebkitBackdropFilter: "blur(6px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 37.5%, rgba(0, 0, 0, 0) 50%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 1) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 0) 87.5%)",
         }}
       />
       <div
@@ -104,11 +94,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 6,
           backdropFilter: "blur(10px)",
-          WebkitBackdropFilter: "blur(10px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 0) 100%)",
+          WebkitBackdropFilter: "blur(10px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 0) 37.5%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 62.5%, rgba(0, 0, 0, 1) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 0) 100%)",
         }}
       />
       <div
@@ -120,11 +108,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 7,
           backdropFilter: "blur(14px)",
-          WebkitBackdropFilter: "blur(14px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 1) 100%)",
+          WebkitBackdropFilter: "blur(14px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 12.5%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 37.5%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 75%, rgba(0, 0, 0, 1) 87.5%, rgba(0, 0, 0, 1) 100%)",
         }}
       />
       <div
@@ -136,11 +122,9 @@ const GradientBlur = () => {
           bottom: 0,
           zIndex: 8,
           backdropFilter: "blur(18px)",
-          WebkitBackdropFilter: "blur(18px)", // Vendor prefix for Safari
-          maskImage:
-            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(0, 0, 0, 1) 100%)",
+          WebkitBackdropFilter: "blur(18px)",
           WebkitMaskImage:
-            "linear-gradient(to top, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) 12.5%)", // Adjusted gradient for Safari
+            "linear-gradient(to bottom, rgba(0, 0, 0, 0) 87.5%, rgba(0, 0, 0, 1) 100%)",
         }}
       />
     </div>
